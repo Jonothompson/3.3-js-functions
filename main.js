bd41887cf1da5a091f0dd4be73514c42eaa130e7
Jonathan Thompson <JonathanLeighThompson@gmail.com>
2015-05-20 18:13:29 -0400

    have some of the problems in the js functions assignments working.

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-/**
- *
- */
-
 // ---------------------
 // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
 // ---------------------
@@ -41,68 +37,106 @@ console.assert( maxOfThree(4, 4, 4) === 4 , "maxOfThree");
 
 function isVowel(char){
     "use strict";
-    if ("a") {
-        return (char) +  "is a vowel";
-    }
-    else if ("e") {
-        return (char) + "is a vowel";
-    }
-    else if ("i") {
-        return (char) + "is a vowel";
-    }
-    else if ("o") {
-        return (char) + "is a vowel";
-    }
-    else if ("u") {
-        return (char) + "is a vowel";
-    }
-    else if ("y") {
-        return (char) + "is kinda a vowel";
-    }
-    else {
-        return (char) + "is not a vowel";
+    if ("a" || "e" || "i" || "o" || "u") {
+        return true;
+    } else {
+        return false;
     }
 }
+    
+//    if ("a") {
+//        return (char) +  "is a vowel";
+//    }
+//    else if ("e") {
+//        return (char) + "is a vowel";
+//    }
+//    else if ("i") {
+//        return (char) + "is a vowel";
+//    }
+//    else if ("o") {
+//        return (char) + "is a vowel";
+//    }
+//    else if ("u") {
+//        return (char) + "is a vowel";
+//    }
+//    else if ("y") {
+//        return (char) + "is kinda a vowel";
+//    }
+//    else {
+//        return (char) + "is not a vowel";
+//    }
+//}
 
-console.assert( isVowel("a") === "a" , "Not a Vowel");
-console.assert( isVowel("e") === "e" , "Not a Vowel");
-console.assert( isVowel("i") === "i" , "Not a Vowel");
-console.assert( isVowel("o") === "o" , "Not a Vowel");
-console.assert( isVowel("u") === "u" , "Not a Vowel");
-console.assert( isVowel("y") === "y" , "Not really a Vowel");
+console.assert( isVowel("a") , "Not a Vowel");
+console.assert( isVowel("e") , "Not a Vowel");
+console.assert( isVowel("i") , "Not a Vowel");
+console.assert( isVowel("o") , "Not a Vowel");
+console.assert( isVowel("u") , "Not a Vowel");
+
 
 // ---------------------
 // Write a function translate() that will translate a text into "rövarspråket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
 // ---------------------
 
-function rovarspraket(phrase){
-    "use strict";
-    //...
-}
+//function rovarspraket(phrase) {
+//    "use strict";
+//    var newPhrase = "";
+//    var phraseLength = phrase.length;
+//    for (var i=0; i < phraseLength; i++) {
+//        var char = phrase.substr(i,1);
+//        var order = ord(char);
+//        if (order>90) {
+//            order -= 32;
+//        }
+//        if ((order >= 65 && order <= 90)) {
+//            if (order != 65 && order != 69 && order != 73 && order != 79 && order != 85) {
+//                char = char + "o" + char;
+//            }
+//        }
+//        newPhrase += char;
+//}
+//return newPhrase;
+//}
+//
+//console.assert(rovarspraket("this is fun") , "I hate this!");
+
 
 // ---------------------
 // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
 // ---------------------
 
-function sum(){
+function sum(numbers){
     "use strict";
-    //...
+    var digits = 0;
+    for (var i = 0; i < numbers.length; i++) {
+        digits += numbers[i];
+    } 
+    return digits;
 }
 
-function multiply(){
+function multiply(numbers){
     "use strict";
-    //...
+    var digits = 1;
+    for (var i = 1; i < numbers.length; i++) {
+        digits *= numbers[i]
+    }
+    return digits;
 }
+
+console.assert( sum([1,2,3,4]) , "nope, try again");
+console.assert( multiply([1,2,3,4]) , "nope, try agian");
 
 // ---------------------
 // Define a function reverse() that computes the reversal of a string. For example, reverse("jag testar") should return the string "ratset gaj".
 // ---------------------
 
-function reverse(){
+function reverse(jag){
     "use strict";
-    //...
+    var strang = " jag testar";
+    return strang.split("").reverse().join("");
 }
 
+console.assert( reverse("jag testar") , "nope nope nope!")
 // ---------------------
 // Write a function findLongestWord() that takes an array of words and returns the length of the longest one.
 // ---------------------
